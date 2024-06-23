ddf87d336e436463af219056b6449617ede19c49
Aman Chokshi <amanchokshi.iisc@gmail.com>
2024-06-23 19:50:19 +1000

    disabling neogit - it may be glitchy?

--- lua/osgiliath/plugins/git.lua
+++ lua/osgiliath/plugins/git.lua
@@ -6,18 +6,18 @@ return {
   {
     'https://github.com/tpope/vim-fugitive',
   },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua', -- optional
-    },
-    config = true,
-  },
+  -- {
+  --   'NeogitOrg/neogit',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim', -- required
+  --     'sindrets/diffview.nvim', -- optional - Diff integration
+  --
+  --     -- Only one of these is needed, not both.
+  --     'nvim-telescope/telescope.nvim', -- optional
+  --     'ibhagwan/fzf-lua', -- optional
+  --   },
+  --   config = true,
+  -- },
   {
     'lewis6991/gitsigns.nvim',
     opts = {
